53b5a2274d53a8cc7edf48cec155d28cc6ecb162
David Alex Souza Santos <davidalex989@gmail.com>
2023-11-22 07:28:04 -0300

    add

---
+++ aulas/aula066.py
@@ -0,0 +1,23 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+
+
+"""def soma(x, y, z):
+    # Definição
+    print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
+
+
+soma(1, 2, 3)
+soma(1, y=2, z=5)
+
+print(1, 2, 3, sep='-')"""
+
+def soma(x, y, z):
+    print(f'{x=} {y=} {z=}', '|', x + y + z)
+
+soma(1, 2, 3)
+
+print(1, 2, 3, sep='-')
